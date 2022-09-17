94fdfcde849dff7822ce2bb07f25b343c13291c4
Eric Chen <ericchen314@gmail.com>
2022-09-17 19:45:10 -0400

    Add authentication packages

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/v1/', include('api.urls')),
     path('', include('course.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/v1/dj-rest-auth/registration/', 
+        include('dj_rest_auth.registration.urls'))
 ]
